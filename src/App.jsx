7cdb7c1026ea82a1c681732d58694171117054f1
sudip-dhakal <dhakalsudip900@gmail.com>
2025-03-08 12:44:28 +0545

    necessary fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Category from "./Homepage/Category";
 import JobListing from "./Homepage/JobListing";
 import Footer from "./Homepage/Footer";
 import Navbar from "./Homepage/Navbar";
-// import useGetAlljob from "./Hooks/useGetAlljob.jsx";
+import useGetAlljob from "./Hooks/useGetAlljob.jsx";
 import itemContext from "./Store/store";
 import FeaturedJobs from "./ReactComponent/FeaturedJobs";
 import CategoriesList from "./ReactComponent/CategoriesList";
@@ -16,7 +16,7 @@ import RecommendedJobs from "./ReactComponent/RecommendedJobs";
 
 export default function App() {
   let { user, setUser } = useContext(itemContext);
-  // useGetAlljob();
+  useGetAlljob();
   console.log(user);
 
   return (
